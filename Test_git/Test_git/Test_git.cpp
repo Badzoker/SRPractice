0ca64879af300d9c6b93c0a8a330f9c0518371c3
Badzoker <jonghan0906@naver.com>
2024-09-02 15:06:45 +0900

    계산기 Frame

--- Test_git/Test_git/Test_git.cpp
+++ Test_git/Test_git/Test_git.cpp
@@ -1,13 +1,74 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+
 using namespace std;
+
+
+float Plus(float _x, float _y);
+
+
 int main()
 {
     cout << "Hello World!\n";
     cout << "각자 자신의 이름을 쓰시오." << endl;
     cout << "이름 : 김유빈";
     cout << "이름 : 이종한";
-
+    
     cout << "이름 : 김유빈";
     cout << "이름 : 정연욱";
+
+
+    cout << "계산기" << endl;
+
+    int a(0);
+    float x(0.f), y(0.f), z(0.f);
+    cout << "1번째 값 :  ";
+    cin >> x;
+    cout << "2번째 값 :  ";
+    cin >> y;
+
+    cout << "어떤 계산을 할 것인가" << endl;
+    cout << "1.더하기 2.빼기 3.곱하기 4.나누기 " << endl;
+    cin >> a;
+
+
+    switch (a)
+    {
+    case 1:
+         z = Plus(x, y);
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    default:
+        break;
+    }
+
+    cout << "계산한 값은 : " << z << endl;
+}
+
+float Plus(float _x, float _y)
+{
+    //이종한
+    return _x + _y;
+}
+
+float Minus(float _x, float _y)
+{
+    //강범승
+    return 0;
+}
+
+float Multi(float _x, float _y)
+{
+    //정연욱
+    return 0;
+}
+
+float Divine(float _x, float _y)
+{
+    //김유빈
+    return 0;
 }
